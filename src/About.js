46c0a7b935ca5b5c162e6914dad3198d96255914
missiveronica <veronica_glu@yahoo.com>
2024-08-16 14:04:55 -0400

    first commit

--- src/About.js
+++ src/About.js
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
+
 function About () {
+
+    const [quote, setQuote] =useState("");
+    useEffect (() => {
+        getQuote();
+
+    }, [])
+    const getQuote  = async() => {
+        const response = await fetch (`https://api.quotable.io/random`);
+        const data = await response.json();
+        setQuote(data.content)
+    }
     return (
         <div>
         <section id ="about">
             <h2>WHO WE ARE</h2>
-          <p>Personalized.</p>
-          <p>Efficient.</p>
-          <p>Caring.</p>
-          <p>Committed.</p>
+          <p className="title">Personalized.</p>
+          <p className="title">Efficient.</p>
+          <p className="title">Caring.</p>
+          <p className="title">Committed.</p>
         </section>
+        <div>
+           <p >{quote}</p> 
+           <button onClick={getQuote}>Get My Daily Motivation Now</button>
         </div>
+        </div>
+
     )
 }
 export default About;
--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   return (
     <div className="App">
       <NavGrid/>
+      <section id ="home">
       <div>
       <video autoplay ="true" muted loop>
         <source src = {cover} type="video/mp4"/>      
         </video>
         <h1 className='header'>Ready to take the Plunge into Hospitality Career?</h1>
         </div>
+        </section>
 <About/>
 
 
