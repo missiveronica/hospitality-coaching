7cbf91d870a3ef98d97047f2a32af19b07f1d1d6
missiveronica <veronica_glu@yahoo.com>
2024-08-21 09:29:21 -0400

    first commit

--- src/Team.js
+++ src/Team.js
@@ -1,11 +1,25 @@
-
+import { useEffect} from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Team () {
+
+    useEffect(() => {
+        AOS.init();
+      }, [])
+
     return(
         <div>
             <section id ="team">
                 <div className="teamContainer">
                     <h2>OUR TEAM</h2>
+                </div>
+                <div data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                className="teamDescription">
+                    <p className="teamText">Our people are at heart of the consulting. Enthusiasme, dynamic mindset, creativity and unique approach to every client make the difference. We are thrilled to share our valuable knowledge of industry and impactful techniques how to win the perfect employer. </p>
+
                 </div>
 
 
